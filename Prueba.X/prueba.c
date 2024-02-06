646496b0887f61c07d56352f2b0eaa91c719ea98
Giancarlo <giancarlo.t0811@gmail.com>
2024-02-06 18:08:59 -0400

    pic config template

---
+++ Prueba.X/prueba.c
@@ -0,0 +1,27 @@
+/*
+ * File:   prueba.c
+ * Author: Giancarlo
+ *
+ * Created on 6 de febrero de 2024, 05:40 PM
+ */
+
+// CONFIGURACION DEL PIC
+#pragma config PLLDIV = 2, CPUDIV = OSC1_PLL2, USBDIV = 2  
+#pragma config FOSC = HSPLL_HS, FCMEN = OFF, IESO = OFF      
+#pragma config PWRT = OFF, BOR = OFF, BORV = 3, VREGEN = OFF    
+#pragma config WDT = OFF
+#pragma config WDTPS = 32768    
+#pragma config CCP2MX = ON, PBADEN = OFF, LPT1OSC = OFF, MCLRE = ON       
+#pragma config STVREN = ON, LVP = OFF, ICPRT = OFF, XINST = OFF      
+#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF   
+#pragma config CPB = OFF, CPD = OFF       
+#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF     
+#pragma config WRTC = OFF, WRTB = OFF, WRTD = OFF      
+#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF     
+#pragma config EBTRB = OFF  
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
