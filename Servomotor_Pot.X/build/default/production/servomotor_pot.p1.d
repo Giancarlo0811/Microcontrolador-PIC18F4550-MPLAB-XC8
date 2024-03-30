5775384367fe842a98b79539f058e4d982b9615c
Giancarlo <giancarlo.t0811@gmail.com>
2024-03-30 10:16:55 -0400

    servomotor pot

---
+++ Servomotor_Pot.X/build/default/production/servomotor_pot.p1.d
@@ -0,0 +1,5 @@
+build/default/production/servomotor_pot.p1:  \
+servomotor_pot.c  \
+adc.h  \
+map_function.h  \
+servo.h 
---
+++ Servomotor_Pot.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Servomotor_Pot.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Servomotor_Pot.X.production.hex
